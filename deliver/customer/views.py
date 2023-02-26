272f3463a36824cc8a3665e88e849657c010667c
Thalisson Castro <adaothalisson@hotmail.com>
2023-02-26 16:58:27 -0300

    create  models to customer order

--- deliver/customer/views.py
+++ deliver/customer/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
+from deliver.customer.models import OrderModel
+from deliver.customer.models import MenuItem
+
 class Index(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/index.html')
@@ -8,4 +11,58 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
+    
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        #get every item from each category
+        lanches = MenuItem.objects.filter(category__name__contains='Lanches')
+        salgados = MenuItem.objects.filter(category__name__contains='Salgados')
+        bebidas = MenuItem.objects.filter(category__name__contains='Bebidas')
+        sobremesas = MenuItem.objects.filter(category__name__contains='Sobremesas')
+        
+        #pass into context
+        context ={
+            'lanches': lanches,
+            'salgados': salgados,
+            'bebidas': bebidas,
+            'sobremesas': sobremesas,
+        }
+
+        #render the template
+        return render(request,'customer/order.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items':[]
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0 
+            item_ids = []
+
+            for item in order_items['items']:
+                price += item['price']
+                item_ids.append(item['id'])
+
+            order = OrderModel.objects.create(price=price)
+            order.items.add(*item_ids)
+
+            context = {
+                'items': order_items['items'],
+                'price': price,
+            }
+
+            return render(request, 'customer/order_confirmation.html', context)
 
--- deliver/deliver/settings.py
+++ deliver/deliver/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
